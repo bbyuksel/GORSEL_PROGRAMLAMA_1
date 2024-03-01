ff834d4afe44fdcf6ac0d36d2073b623c531c3fb
yukselbestami@hotmail.com <BBYclub20+>
2024-03-01 17:19:01 +0300

    29_Subat

--- MESAFE HESAPLAYICI/Form1.Designer.cs
+++ MESAFE HESAPLAYICI/Form1.Designer.cs
@@ -40,9 +40,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 9);
+            this.label1.Location = new System.Drawing.Point(22, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.Size = new System.Drawing.Size(158, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Hız Giriniz: (km/saat):";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -50,59 +51,65 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Location = new System.Drawing.Point(18, 75);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.Size = new System.Drawing.Size(166, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Seyahat Süresi (saat):";
             // 
             // txtSpeed
             // 
-            this.txtSpeed.Location = new System.Drawing.Point(128, 6);
+            this.txtSpeed.Location = new System.Drawing.Point(192, 9);
+            this.txtSpeed.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtSpeed.Name = "txtSpeed";
-            this.txtSpeed.Size = new System.Drawing.Size(100, 20);
+            this.txtSpeed.Size = new System.Drawing.Size(148, 26);
             this.txtSpeed.TabIndex = 2;
             // 
             // txtTime
             // 
-            this.txtTime.Location = new System.Drawing.Point(128, 42);
+            this.txtTime.Location = new System.Drawing.Point(192, 65);
+            this.txtTime.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtTime.Name = "txtTime";
-            this.txtTime.Size = new System.Drawing.Size(100, 20);
+            this.txtTime.Size = new System.Drawing.Size(148, 26);
             this.txtTime.TabIndex = 3;
             // 
             // txtCalculate
             // 
-            this.txtCalculate.Location = new System.Drawing.Point(44, 85);
+            this.txtCalculate.Location = new System.Drawing.Point(26, 131);
+            this.txtCalculate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtCalculate.Name = "txtCalculate";
-            this.txtCalculate.Size = new System.Drawing.Size(155, 31);
+            this.txtCalculate.Size = new System.Drawing.Size(314, 48);
             this.txtCalculate.TabIndex = 4;
-            this.txtCalculate.Text = "MESAFE HESAPLA";
+            this.txtCalculate.Text = "MESAFEYİ HESAPLA";
             this.txtCalculate.UseVisualStyleBackColor = true;
             this.txtCalculate.Click += new System.EventHandler(this.txtCalculate_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(41, 137);
+            this.label3.Location = new System.Drawing.Point(64, 203);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.Size = new System.Drawing.Size(84, 20);
             this.label3.TabIndex = 0;
             this.label3.Text = "Alınan Yol:";
             this.label3.Click += new System.EventHandler(this.label1_Click);
             // 
             // txtDistance
             // 
-            this.txtDistance.Location = new System.Drawing.Point(104, 130);
+            this.txtDistance.Location = new System.Drawing.Point(156, 200);
+            this.txtDistance.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtDistance.Name = "txtDistance";
             this.txtDistance.ReadOnly = true;
-            this.txtDistance.Size = new System.Drawing.Size(100, 20);
+            this.txtDistance.Size = new System.Drawing.Size(184, 26);
             this.txtDistance.TabIndex = 5;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(250, 167);
+            this.ClientSize = new System.Drawing.Size(375, 257);
             this.Controls.Add(this.txtDistance);
             this.Controls.Add(this.txtCalculate);
             this.Controls.Add(this.txtTime);
@@ -110,6 +117,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
