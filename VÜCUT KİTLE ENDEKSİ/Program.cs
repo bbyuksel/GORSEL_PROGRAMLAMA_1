3827b2b991f441be8ff35802c11357e4a64a9b3c
bbyuksel <yukselbestami@hotmail.com>
2024-03-07 15:54:41 +0300

    last

---
+++ VÜCUT KİTLE ENDEKSİ/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection.Metadata;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Kilonuzu giriniz: ");
+        double kilo = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Boyunuzu giriniz: ");
+        double boy = Convert.ToDouble(Console.ReadLine());
+        boy /= 100; // boy = boy / 100
+
+        double vki = kilo / (boy * boy);
+        Console.WriteLine("Vücut kitle indeksiniz: " + vki.ToString("N2"));
+    }
+}
