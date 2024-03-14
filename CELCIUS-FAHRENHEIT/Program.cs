840af4415121af6bd256ff30e0e9f00b643397d2
bbyuksel <yukselbestami@hotmail.com>
2024-03-14 14:05:55 +0300

    Ders_3

---
+++ CELCIUS-FAHRENHEIT/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Celcius\t\tFahrenheit");
+        Console.WriteLine("-----------------------");
+        for (int C = 0; C <= 20; C++) 
+        {
+            double F = (double)9 / 5 * C + 32;
+            Console.WriteLine($"{C}\t\t{F:N1}");
+        }
+    }
+}
---
+++ MESAFE HESAPLA YAZ/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+class Program
+{
+    static void Main()
+    {
+        StreamWriter yaz = new StreamWriter("mesafeler.txt");
+        Console.WriteLine("Lütfen aracın hızını (km/saat) giriniz:");
+        int hiz = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Lütfen seyahat süresini (saat) giriniz:");
+        int saat = Convert.ToInt32(Console.ReadLine());
+
+        //her saat kat edilen mesafeyi ekrana yaz
+        for (int i = 1; i <= saat; i++)
+        {
+            int mesafe = hiz * i;
+
+            //ekrana yaz
+            Console.WriteLine($"{i} saat sonunda gidilen mesafe {mesafe} km");
+
+            //dosyaya yaz
+            yaz.WriteLine($"{i} saat sonunda gidilen mesafe {mesafe} km");
+
+            //  Thread.Sleep(1000); // Her çıktı için 1 sn bekle            
+        }
+        yaz.Close();
+    }
+}
